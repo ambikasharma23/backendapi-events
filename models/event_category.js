a80a004dff0c4e4a31b93e095f458e53d1fe0bf7
Ambika Sharma <ambika@eazydiner.com>
2024-05-07 10:00:10 +0530

    new models added

---
+++ models/event_category.js
@@ -0,0 +1,35 @@
+module.exports=(sequelize, DataTypes)=>{
+    const event_category = sequelize.define("event_category",{
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name:{
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        icon:{
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        created_by: {
+            type: DataTypes.STRING,
+          },
+          updated_by: {
+            type: DataTypes.STRING,
+          },
+          createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            field: 'created_at',
+          },
+          updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            field: 'updated_at',
+          },
+
+    });
+    return event_category;
+}
